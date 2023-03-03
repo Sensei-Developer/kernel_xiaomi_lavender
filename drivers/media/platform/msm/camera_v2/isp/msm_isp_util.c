10233d816d200055f9eeac2c6a3d10b3814d7a51
Sensei-Developer <shoppingplay32@gmail.com>
2023-03-03 21:53:39 +0300

    Revert "msm: camera: Optimize memory allocation for small buffers" This reverts commit 3ba104f6b522a62d9fd0c7d6fee0d36ca9b02dbc

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -1476,7 +1476,6 @@ int msm_isp_proc_cmd(struct vfe_device *vfe_dev, void *arg)
 	reg_cfg_cmd = kzalloc(sizeof(struct msm_vfe_reg_cfg_cmd)*
 		proc_cmd->num_cfg, GFP_KERNEL);
 	if (!reg_cfg_cmd) {
-		pr_err("%s: reg_cfg alloc failed\n", __func__);
 		rc = -ENOMEM;
 		goto reg_cfg_failed;
 	}
